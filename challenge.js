039909d2e87cfe0a1b125554e4d1f1956fb0d3a1
t-bello7 <bello4aus@gmail.com>
2022-07-10 18:52:15 +0100

    solve coding challenge

--- challenge.js
+++ challenge.js
@@ -1,7 +1,24 @@
 function balancedBrackets(string) {
   // your code here
+  string = string.replace(/[\w]/g,'');
+  let stringArr = [...string]
+  let balancedStack = [];
+  stringArr.forEach((element) => {
+    if('({['.includes(element)){
+      balancedStack.push(element);
+    }
+    else{
+      if ((balancedStack[balancedStack.length-1] === '{' && element === '}') || (balancedStack[balancedStack.length-1] === '(' && element === ')') || (balancedStack[balancedStack.length-1] === '[' && element === ']')){
+        balancedStack.pop()
+      }
+    }
+  }); 
+  if (balancedStack.length === 0){
+    return true;
+  }
+  return false;
+  
 }
-
 console.log(balancedBrackets('(hello)[world]'))
 // => true
 
